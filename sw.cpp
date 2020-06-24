00c054085b9b0856c1991125934abad73a82c0dc
jaykumarkhalote <54491468+jaykumarkhalote@users.noreply.github.com>
2020-06-24 12:54:33 +0530

    Create sw.cpp

---
+++ sw.cpp
@@ -0,0 +1,50 @@
+#include<conio.h>
+#include<iostream.h>
+
+void main()
+  {  clrscr();
+     int m;
+     cout<<"\t\tFIND MONTHN\n\n";
+     cout<<"Enter the month number : ";
+     cin>>m;
+
+     if (m==1)
+     cout<<"Janury"<<endl;
+     else if (m==2)
+     cout<<"febury"<<endl;
+
+     else if (m==3)
+     cout<<"March"<<endl;
+
+     else if (m==4)
+     cout<<"April"<<endl;
+
+     else if (m==5)
+     cout<<"May"<<endl;
+
+     else if (m==6)
+     cout<<"June"<<endl;
+
+     else if (m==7)
+     cout<<"July"<<endl;
+
+     else if (m==8)
+     cout<<"August"<<endl;
+
+     else if (m==9)
+     cout<<"September"<<endl;
+
+     else if (m==10)
+     cout<<"October"<<endl;
+
+     else if (m==11)
+     cout<<"Nubember"<<endl;
+
+     else if (m==12)
+     cout<<"December"<<endl;
+
+     else
+     cout<<"Wrong month number "<<endl;
+
+    getch();
+  }
